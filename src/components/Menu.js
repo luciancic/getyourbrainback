97224e84855ae01c39797ce263205df934a595c6
luciancic <luciancondrea95@gmail.com>
2018-06-30 18:11:15 -0400

    Add basic markup to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
-    return <h1>Menu</h1>
+    return <div id='menu'>
+        <h1>Get Your Brain Back!</h1>
+        <Link to='/game' ><button>Play Game</button></Link>
+        <Link to='/tutorial' ><button>Play Tutorial</button></Link>
+        <Link to='/settings' ><button>Settings</button></Link>
+    </div>
 }
