b7d406642e2899eda83e3dc3dcf5bd9f2c566377
Lucian Condrea <luciancondreadev@gmail.com>
2019-03-18 16:03:02 -0400

    Fix link security risk in menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ class Menu extends Component {
         return <div id='menu' className='center'>
             <section id='menu-text'>
                 <h1>Get Your Brain Back!</h1>
-                <p>Challenge your brain and improve your working memory. Inspired by <a href='https://wikipedia.org/wiki/N-back' target='_blank'>n-back</a>.</p>
+                <p>Challenge your brain and improve your working memory. Inspired by <a href='https://wikipedia.org/wiki/N-back' target='_blank' rel="noopener noreferrer">n-back</a>.</p>
             </section>
             <section id='menu-buttons'>
                 <Link to='/game'><button className='btn green'  onClick={this.props.startGame}>Play Game</button></Link>
