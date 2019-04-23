f7cb5ba0e8dc57fd3ff31a3556b1dd4d45ee5899
Lucian Condrea <luciancondreadev@gmail.com>
2019-04-23 00:39:19 -0400

    Move Provider down into App

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
 import Menu from './Menu';
 import Game from './game/Game';
 import Results from './Results';
@@ -8,20 +10,18 @@ import Tutorial from './tutorial/Tutorial';
 import 'materialize-css/dist/css/materialize.min.css';
 import './App.css'; // Global styles
 
-import store from '../store';
-
-class App extends Component {
+export default class App extends Component {
   render() {
-    return <BrowserRouter>
-      <div>
-        <Route exact path='/' component={Menu}/>
-        <Route path='/game' component={Game}/>
-        <Route path='/results' component={Results}/>
-        <Route path='/settings' component={Settings}/>
-        <Route path='/tutorial' component={Tutorial}/>
-      </div>
-    </BrowserRouter>
+    return <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' component={Menu}/>
+          <Route path='/game' component={Game}/>
+          <Route path='/results' component={Results}/>
+          <Route path='/settings' component={Settings}/>
+          <Route path='/tutorial' component={Tutorial}/>
+        </div>
+      </BrowserRouter>
+    </Provider>
   }
 }
-
-export default App
