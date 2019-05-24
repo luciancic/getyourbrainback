1795a8728dd0250acab7d018fca6b237e9b1ee4a
Lucian Condrea <luciancondreadev@gmail.com>
2019-05-25 00:10:12 +0200

    Refactor Provider and App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Provider from '../context';
 import Menu from '../components/Menu';
@@ -9,18 +9,16 @@ import TutorialContainer from './TutorialContainer';
 import 'materialize-css/dist/css/materialize.min.css';
 import './App.css'; // Global styles
 
-export default class App extends Component {
-  render() {
-    return <Provider>
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Menu}/>
-          <Route path='/game' component={Game}/>
-          <Route path='/results' component={Results}/>
-          <Route path='/settings' component={SettingsContainer}/>
-          <Route path='/tutorial' component={TutorialContainer}/>
-        </div>
-      </BrowserRouter>
-    </Provider>
-  }
+export default function App() {
+  return <Provider>
+    <BrowserRouter>
+      <Fragment>
+        <Route exact path='/' component={Menu}/>
+        <Route path='/game' component={Game}/>
+        <Route path='/results' component={Results}/>
+        <Route path='/settings' component={SettingsContainer}/>
+        <Route path='/tutorial' component={TutorialContainer}/>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 }
