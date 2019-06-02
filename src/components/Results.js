e0b29777c6102435cfc22bfea2b8a155ea719aa0
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-02 15:30:11 +0300

    implement results context

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import escapeable from './escapeable';
+import React from 'react'
+import escapeable from './escapeable'
 
-export const Results = () => {
-    return <h1>Results</h1>
+export function Results() {
+    return <div>
+        <h1>Results</h1>
+        <p>Results Body</p>
+        <div>
+            <button>Play Again</button>
+            <button>Menu</button>
+        </div>
+    </div>
 }
 
-export default escapeable(Results);
+export default escapeable(Results)
--- src/context/ResultsContext.js
+++ src/context/ResultsContext.js
@@ -1,12 +1,48 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from 'react'
+import { LocalStorage } from '../utils'
 
 const ResultsContext = createContext()
 
-export default ResultsContext
+// Result object anatomy:
+// 
+// const result = {
+//     n: 2, 
+//     maxRounds: 20,
+//     score: {
+//         positions: {
+//             correct: 7,
+//             mistakes: 2
+//         },
+//         letters: {
+//             correct: 3,
+//             mistakes: 4
+//         }
+//     }
+// }
 
 export function ResultsProvider(props) {
-    const state = useState({})
-    return <ResultsContext.Provider value={state}>
+    const storedRecords = LocalStorage.get('records')
+    
+    const initialRecords = storedRecords || []
+    
+    const [records, __setRecords__] = useState(initialRecords)
+    
+    function addRecord(record) {
+        const newRecords = records.slice(0)
+
+        // Only keep the last 20 game results
+        if (newRecords.length === 20) newRecords.shift()
+        newRecords.unshift(record)
+
+        __setRecords__(newRecords)
+        LocalStorage.set('records', newRecords)
+    }
+    
+    const value = [records, addRecord]
+    
+    return <ResultsContext.Provider value={value}>
         {props.children}
     </ResultsContext.Provider>
 }
+
+export default ResultsContext
