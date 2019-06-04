255d1d767c1f45da3b5e7b9bffc221fbba17c180
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-04 11:23:49 +0200

    Implement results ui functionality

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import escapeable from './escapeable'
+import ResultsContext from '../context/ResultsContext'
 
 export function Results() {
+    const [ res ] = useContext(ResultsContext)
+    const lastResult = Array.isArray(res) ? res[0] : []
+
     return <div>
         <h1>Results</h1>
-        <p>Results Body</p>
+        { lastResult ? 
+            <p>Last game results: { calculatePercentage(lastResult.score) }%</p> 
+            :
+            <p>No results yet, play a game!</p>
+        }
         <div>
-            <button>Play Again</button>
-            <button>Menu</button>
+            <Link to='/game'><button className='btn green'>{ lastResult ? 'Play again' : 'Play' }</button></Link>
+            <Link to='/'><button className='btn orange'>Menu</button></Link>
         </div>
     </div>
+}
+
+function calculatePercentage(result) {
+    const cp = result.positions.correct
+    const mp = result.positions.mistakes
+    const cl = result.letters.correct
+    const ml = result.letters.mistakes
+    const total = cp + mp + cl + ml
+
+    return Math.round( (cp + cl) / total * 100 )
 }
 
 export default escapeable(Results)
