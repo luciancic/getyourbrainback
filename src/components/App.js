43f0912c4ae5a6137cd6d6de3223fa2bc6a99d94
Lucian Condrea <luciancondreadev@gmail.com>
2019-06-24 11:22:55 -0400

    disable materialize styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ContextProvider from '../context'
 import Router from './Router'
 
-import 'materialize-css/dist/css/materialize.min.css'
 import '../styles/styles.scss'
 
 function App() {
